fa61128c6442d250f74b61139d89cd71f1390a6e
Prathmesh Salve <168015208+PRATHMESH-SALVE@users.noreply.github.com>
2024-06-18 15:23:00 +0530

    Update tasks on set

--- task1.py
+++ task1.py
@@ -1,3 +1,38 @@
 my_list = [64, 34, 25, 12, 22, 11, 90]
 sorted_list = sorted(my_list)
 print(sorted_list)
+Q2
+numbers = [10, 5, 20, 8, 15]
+average = sum(numbers) / len(numbers)
+print(average),
+
+Q3
+p_num ={2,3,5,7,11}
+p_num.add(13)
+p_num.add(17)
+print(p_num)
+
+Q4
+even_num={2,4,6,8,10}
+print(even_num)
+
+Q5
+prime_num={2,3,5,7,11,13,17}
+even_num={2,4,6,8,10}
+intersection=prime_num.intersection(even_num)
+print(intersection)
+
+Q6
+prime_num={2,3,5,7,11,13,17}
+even_num={2,4,6,8,10}
+union=prime_num.union(even_num)
+print(union)
+
+Q7
+prime_num = {2,3,5,7,11,13,17}
+even_num = {2,4,6,8,10}
+difference = prime_num.difference(even_num)
+print(difference)
+
+
+
